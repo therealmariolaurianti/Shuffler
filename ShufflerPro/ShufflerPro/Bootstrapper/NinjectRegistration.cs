0ec8649862ceec0b79d4fda1211867c9e96b596b
Mario Laurianti <mlaurianti24@gmail.com>
2017-03-17 14:11:24 -0400

    framework

---
+++ ShufflerPro/ShufflerPro/Bootstrapper/NinjectRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Bootstrap.Ninject;
+using Ninject;
+
+namespace ShufflerPro.Bootstrapper
+{
+    public class NinjectRegistration : INinjectRegistration
+    {
+        public void Register(IKernel container)
+        {
+            
+        }
+    }
+}
