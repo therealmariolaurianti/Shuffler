70263420f2a508d3a2055e0303906f3a443a15e0
Mario Laurianti <mlaurianti24@gmail.com>
2024-11-06 13:38:07 -0500

    Add extension method for hashing song properties
    Moved the hash computation to a static extension method `Hash` in `SongExtensions`. This refactors the `Song` constructor for cleaner and more modular code.

--- ShufflerPro/ShufflerPro.Client/Entities/Song.cs
+++ ShufflerPro/ShufflerPro.Client/Entities/Song.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-using File = TagLib.File;
+﻿using File = TagLib.File;
 
 namespace ShufflerPro.Client.Entities;
 
@@ -18,22 +16,14 @@ public class Song : EntityBase
         Path = path;
         Time = songFile?.Properties.Duration.ToString("mm':'ss");
         Duration = songFile?.Properties.Duration;
-
-        var input = $"{path}{Title}{Artist}{Album}{Time}";
-        using (var md5 = MD5.Create())
-        {
-            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
-            var result = new Guid(hash);
-            
-            Id = result;
-        }
+        Id = this.Hash(path);
     }
 
     public Guid Id { get; }
     public string? Genre { get; }
     public string? Title { get; }
     public int? Track { get; }
-    public string Artist { get; private set; }
+    public string Artist { get; }
     public string Album { get; }
     public string? Path { get; set; }
     public string? Time { get; }
---
+++ ShufflerPro/ShufflerPro.Client/SongExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using ShufflerPro.Client.Entities;
+
+namespace ShufflerPro.Client;
+
+public static class SongExtensions
+{
+    public static Guid Hash(this Song song, string path)
+    {
+        var input = $"{path}{song.Title}{song.Artist}{song.Album}{song.Time}";
+        var hash = MD5.HashData(Encoding.UTF8.GetBytes(input));
+        
+        return new Guid(hash);
+    }
+}
