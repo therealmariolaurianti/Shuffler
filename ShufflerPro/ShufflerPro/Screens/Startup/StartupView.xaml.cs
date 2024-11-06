4c85b44921d9474e4e9510e6323769e0bba4f4d4
Mario Laurianti <mlaurianti24@gmail.com>
2024-11-06 14:40:28 -0500

    Add fun fact display and typewrite effect to startup view
    Implemented a randomized fun fact feature in the StartupViewModel and displayed it in the StartupView. Added a typewriter animation for the loading text in the StartupView.xaml.cs. Updated the window style of StartupView for a cleaner appearance.

--- ShufflerPro/ShufflerPro/Screens/Startup/StartupView.xaml.cs
+++ ShufflerPro/ShufflerPro/Screens/Startup/StartupView.xaml.cs
@@ -1,9 +1,47 @@
-﻿namespace ShufflerPro.Screens.Startup;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace ShufflerPro.Screens.Startup;
 
 public partial class StartupView
 {
     public StartupView()
     {
         InitializeComponent();
+        TypewriteTextblock("Calculating....", LoadingTextBlock, TimeSpan.FromSeconds(5));
+    }
+
+    private void TypewriteTextblock(string textToAnimate, TextBlock txt, TimeSpan timeSpan)
+    {
+        var story = new Storyboard
+        {
+            FillBehavior = FillBehavior.HoldEnd,
+            RepeatBehavior = RepeatBehavior.Forever
+        };
+
+        var stringAnimationUsingKeyFrames = new StringAnimationUsingKeyFrames
+        {
+            Duration = new Duration(timeSpan)
+        };
+
+        var tmp = string.Empty;
+        foreach (var c in textToAnimate)
+        {
+            var discreteStringKeyFrame = new DiscreteStringKeyFrame
+            {
+                KeyTime = KeyTime.Paced
+            };
+            
+            tmp += c;
+            discreteStringKeyFrame.Value = tmp;
+            stringAnimationUsingKeyFrames.KeyFrames.Add(discreteStringKeyFrame);
+        }
+
+        Storyboard.SetTargetName(stringAnimationUsingKeyFrames, txt.Name);
+        Storyboard.SetTargetProperty(stringAnimationUsingKeyFrames, new PropertyPath(TextBlock.TextProperty));
+        
+        story.Children.Add(stringAnimationUsingKeyFrames);
+        story.Begin(txt);
     }
 }
--- ShufflerPro/ShufflerPro/Screens/Startup/StartupViewModel.cs
+++ ShufflerPro/ShufflerPro/Screens/Startup/StartupViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using ShufflerPro.Client;
 using ShufflerPro.Client.Controllers;
 using ShufflerPro.Client.Entities;
 using ShufflerPro.Framework.WPF;
@@ -23,9 +24,11 @@ public class StartupViewModel : ViewModelBase
         _windowManager = windowManager;
     }
 
+    public string FunFact => FunFacts.Items.PickRandom();
+
     protected override void OnViewLoaded(object view)
     {
-        RunAsync(async () => await Load());
+        //RunAsync(async () => await Load());
     }
 
     protected override Task OnInitializeAsync(CancellationToken cancellationToken)
@@ -44,12 +47,24 @@ public class StartupViewModel : ViewModelBase
                 .IfSuccess(createdLibrary => library = createdLibrary);
         }).ConfigureAwait(true);
 
-        if (library is null) 
+        if (library is null)
             throw new Exception("Failed to load library.");
 
         var viewModel = _shellViewModelFactory.Create(library);
         await _windowManager.ShowWindowAsync(viewModel);
-        
+
         await TryCloseAsync();
     }
+}
+
+internal static class FunFacts
+{
+    public static List<string> Items =
+    [
+        "Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible.",
+        "Octopuses have three hearts. Two pump blood to the gills, while the third pumps it to the rest of the body.",
+        "Bananas are berries, but strawberries are not! In botanical terms, bananas fit the definition of a berry, but strawberries do not.",
+        "Wombat poop is cube-shaped. This helps prevent it from rolling away and is a part of their unique digestive system.",
+        "Sharks have been around longer than trees! Sharks have existed for over 400 million years, while trees appeared around 350 million years ago."
+    ];
 }
