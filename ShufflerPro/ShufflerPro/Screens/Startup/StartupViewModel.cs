a29cd266b2f43cc93c8881e1d2f7dac3b2c376d6
Mario Laurianti <mlaurianti24@gmail.com>
2024-11-06 14:47:04 -0500

    Enable GIF support and UI enhancements in StartupView
    Uncommented the async call in the StartupViewModel and introduced animated GIF support by adding the WpfAnimatedGif package. Updated the StartupView layout to include a new animated GIF and reorganized elements for better design. Removed unnecessary typewriting animation logic from StartupView.xaml.cs.

--- ShufflerPro/ShufflerPro/Screens/Startup/StartupViewModel.cs
+++ ShufflerPro/ShufflerPro/Screens/Startup/StartupViewModel.cs
@@ -28,7 +28,7 @@ public class StartupViewModel : ViewModelBase
 
     protected override void OnViewLoaded(object view)
     {
-        //RunAsync(async () => await Load());
+        RunAsync(async () => await Load());
     }
 
     protected override Task OnInitializeAsync(CancellationToken cancellationToken)
