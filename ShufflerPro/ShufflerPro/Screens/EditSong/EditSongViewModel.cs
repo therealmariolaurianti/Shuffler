37b3bfa9e70d24d3616f09676113a2313fbee18f
Mario Laurianti <mlaurianti24@gmail.com>
2024-11-07 14:00:16 -0500

    work on editing songs

--- ShufflerPro/ShufflerPro/Screens/EditSong/EditSongViewModel.cs
+++ ShufflerPro/ShufflerPro/Screens/EditSong/EditSongViewModel.cs
@@ -5,10 +5,79 @@ namespace ShufflerPro.Screens.EditSong;
 
 public class EditSongViewModel : ViewModelBase
 {
-    private readonly Song _song;
+    private string? _album;
+    private string? _artist;
+    private string? _genre;
+    private string? _title;
+    private int? _track;
 
     public EditSongViewModel(Song song)
     {
-        _song = song;
+        Title = song.Title;
+        Artist = song.Artist;
+        Album = song.Album;
+        Track = song.Track;
+        Genre = song.Genre;
+
+        Duration = song.Duration!.Value.ToString("mm':'ss");
+        Path = song.Path;
+    }
+
+    public string? Path { get; }
+    public string Duration { get; }
+
+    public string? Genre
+    {
+        get => _genre;
+        set
+        {
+            if (value == _genre) return;
+            _genre = value;
+            NotifyOfPropertyChange();
+        }
+    }
+
+    public int? Track
+    {
+        get => _track;
+        set
+        {
+            if (value == _track) return;
+            _track = value;
+            NotifyOfPropertyChange();
+        }
+    }
+
+    public string? Album
+    {
+        get => _album;
+        set
+        {
+            if (value == _album) return;
+            _album = value;
+            NotifyOfPropertyChange();
+        }
+    }
+
+    public string? Artist
+    {
+        get => _artist;
+        set
+        {
+            if (value == _artist) return;
+            _artist = value;
+            NotifyOfPropertyChange();
+        }
+    }
+
+    public string? Title
+    {
+        get => _title;
+        set
+        {
+            if (value == _title) return;
+            _title = value;
+            NotifyOfPropertyChange();
+        }
     }
 }
