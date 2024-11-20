a88c51a82664a2e49786ff4d658c7d4407a62b9a
Mario Laurianti <mlaurianti24@gmail.com>
2024-11-20 04:50:08 -0500

    Begin add RadioController to stream internet radio stations
    Created a new RadioController class to handle playing internet radio streams via NAudio. Defined two sample radio stations, ChillHop and Lofi, and updated the settings to recognize "lofi" as a dictionary word.

---
+++ ShufflerPro/ShufflerPro.Web/RadioController.cs
@@ -0,0 +1,37 @@
+﻿using NAudio.Wave;
+
+namespace ShufflerPro.Web;
+
+public class RadioController
+{
+    public void StartStation(IRadioStation radioStation)
+    {
+        using (var mediaFoundationReader = new MediaFoundationReader(radioStation.Url))
+        using (var wasapiOut = new WasapiOut())
+        {
+            wasapiOut.Init(mediaFoundationReader);
+            wasapiOut.Play();
+            
+            while (wasapiOut.PlaybackState == PlaybackState.Playing)
+                Thread.Sleep(1000);
+        }
+    }
+}
+
+public interface IRadioStation
+{
+    public string Name { get; }
+    public string Url { get; }
+}
+
+public class ChillHopRadioStation : IRadioStation
+{
+    public string Name => "ChillHop";
+    public string Url => "https://streams.fluxfm.de/Chillhop/mp3-128/streams.fluxfm.de/";
+}
+
+public class LofiRadioStation : IRadioStation
+{
+    public string Name => "Lofi";
+    public string Url => "https://streams.fluxfm.de/Chillhop/aac-64/streams.fluxfm.de/";
+}
