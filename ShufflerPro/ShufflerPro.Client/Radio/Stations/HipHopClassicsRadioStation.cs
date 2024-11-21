065e89211e5170958df81990dcf438d86a1c07cc
Mario Laurianti <mlaurianti24@gmail.com>
2024-11-21 12:40:15 -0500

    Add HipHop Classics Radio Station and update build version
    Introduced a new radio station, "HipHop Classics," with a streaming URL. Additionally, incremented the build version from "121" to "122" in the installation script.

---
+++ ShufflerPro/ShufflerPro.Client/Radio/Stations/HipHopClassicsRadioStation.cs
@@ -0,0 +1,7 @@
+﻿namespace ShufflerPro.Client.Radio.Stations;
+
+public class HipHopClassicsRadioStation : IRadioStation
+{
+    public string Name => "HipHop Classics";
+    public string Url => "https://streams.fluxfm.de/boomfmclassics/aac-64/audio/";
+}
