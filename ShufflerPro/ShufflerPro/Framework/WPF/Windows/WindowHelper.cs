111d4e2d2426f444c705ec7e29d21c6d8c4479e7
Mario Laurianti <mlaurianti24@gmail.com>
2024-12-03 15:29:16 -0500

    fix rounded corners when os is below windows 11

--- ShufflerPro/ShufflerPro/Framework/WPF/Windows/WindowHelper.cs
+++ ShufflerPro/ShufflerPro/Framework/WPF/Windows/WindowHelper.cs
@@ -8,19 +8,30 @@ public static class WindowHelper
 {
     public static void WindowsRoundCorners(Window? window)
     {
-        if (window == null)
+        if (window == null || IsNotWindowsEleven())
             return;
 
         Application.Current.Dispatcher.BeginInvoke(new Action(() =>
         {
             if (!window.IsActive)
                 return;
-            
+
             var hWnd = new WindowInteropHelper(window).EnsureHandle();
             var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
 
             WinImport.DwmSetWindowAttribute(hWnd, DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE, ref preference,
                 sizeof(uint));
         }), DispatcherPriority.Background);
+    }
+
+    private static bool IsNotWindowsEleven()
+    {
+        var version = Environment.OSVersion.Version;
+
+        return version.Major switch
+        {
+            10 when version.Build >= 22000 => false,
+            _ => true
+        };
     }
 }
